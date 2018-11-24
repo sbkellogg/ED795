2def0671dd25d97e245f45653361702f55d7aa84
sbkellogg <sbkellog@ncsu.edu>
2018-11-24 09:57:56 -0500

    readme update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ED795
 Course Files for ED 795 Text Mining in Education 
+
+Syllabus
--- Unit 1.R
+++ Unit 1.R
@@ -1,7 +1,12 @@
 setwd("/Volumes/GoogleDrive/My Drive/External Work/NCSU/ED 795 - Text Mining/R/ED795")
 
+install.packages("tidyverse")
+library(tidyverse)
+
+
 install.packages("gutenbergr")
 library(gutenbergr) 
+
 
 education <- gutenberg_download(c(39863, 48906, 29259), 
                                meta_fields = "author") 
@@ -11,3 +16,5 @@ physics <- gutenberg_download(c(37729, 14725, 13476, 55586),
 
 physics <- gutenberg_download(c(37729, 14725, 13476, 55586), 
                               meta_fields = "author")
+
+
