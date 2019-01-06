3f2f0d2dbd001e6b6e04ef51008f5b293740f3f2
Shaun Kellogg <sbkellog@ncsu.edu>
2019-01-06 12:50:27 -0500

    Add files via upload

---
+++ unti_1_tutorial_1.R
@@ -0,0 +1,42 @@
+## Unit 1 Tutorial 1: Getting Started with R
+
+setwd("/Volumes/GoogleDrive/My Drive/External Work/NCSU/ED 795 - Text Mining/R/ED795")
+
+install.packages("swirl")
+library(swirl)
+ls()
+rm(list=ls())
+swirl()
+Shaun
+5
+1
+
+swirl::install_course("A_(very)_short_introduction_to_R")
+swirl()
+Shaun
+5
+1
+1
+swirl()
+Shaun
+5
+5
+1
+2
+10^2 + 36
+(2017-2011)/(2017-1978)*100
+
+A = 4
+A
+A*5
+A = A + 10
+rm(list=ls())
+B=c(3,4,5)
+(3+4+5)/3
+mean(B)
+mean(B)
+sum(c(4,5,8,11))
+rnorm(10)
+rnorm(10, mean=1.2, sd=3.4)
+?rnorm
+
